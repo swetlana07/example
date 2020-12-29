530383f401df7b7653638c796fce61fe31901e50
Swetlana <osa670807@gmail.com>
2020-12-29 19:03:57 +0500

    My second

--- main.py
+++ main.py
@@ -6,7 +6,9 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    a = input('a=')
+    b = input('b=')
+    print(f'Hi, {a+b}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
